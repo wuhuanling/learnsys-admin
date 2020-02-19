3abb4216268fb61378b54655e78c7a751f56ac32
whl <459634611@qq.com>
2020-02-19 14:40:16 +0800

    Initial commit

---
+++ src/main/java/com/whl/learnsys/LearnsysApplication.java
@@ -0,0 +1,13 @@
+package com.whl.learnsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whl/learnsys/LearnsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whl.learnsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
